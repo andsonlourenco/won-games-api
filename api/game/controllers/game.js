51e8b9ae5ea1d4c757283a0b05b5f28128d26575
Andson Lourenco <andson-lourenco@hotmail.com>
2021-01-24 19:07:58 -0300

    Aula Entendo o ctx (contexto de response/request do Koa)

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -7,7 +7,10 @@
 
 module.exports = {
   populate: (ctx) => {
-    console.log("Inicializing");
-    ctx.send({ ok: true });
+    console.log("Starting to populate...");
+
+    console.log(ctx.query);
+
+    ctx.send("Finished populating!");
   },
 };
