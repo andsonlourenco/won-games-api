2aaf949122da9c24d4f992ac084ac481309253e3
Andson Lourenco <andson-lourenco@hotmail.com>
2021-01-30 22:05:21 -0300

    Aula Buscando dados já criados e evitando erros de duplicação

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -23,6 +23,22 @@ async function getGameInfor(slug) {
   };
 }
 
+async function getByName(name, entityName) {
+  const item = await strapi.services[entityName].find({ name });
+  return item.length ? item[0] : null;
+}
+
+async function create(name, entityName) {
+  const item = await getByName(name, entityName);
+
+  if (!item) {
+    return await strapi.services[entityName].create({
+      name,
+      slug: slugify(name, { lower: true }),
+    });
+  }
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -31,15 +47,10 @@ module.exports = {
       data: { products },
     } = await axios.get(gogApiUrl);
 
-    await strapi.services.publisher.create({
-      name: products[0].publisher,
-      slug: slugify(products[0].publisher).toLowerCase(),
-    });
+    await create(products[1].publisher, "publisher");
+    await create(products[1].developer, "developer");
 
-    await strapi.services.developer.create({
-      name: products[0].developer,
-      slug: slugify(products[0].developer).toLowerCase(),
-    });
+    console.log(await getByName("Bloober Team SA", "developer"));
 
     //console.log(await getGameInfor(products[0].slug));
   },
